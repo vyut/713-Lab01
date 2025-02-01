fcbabb00382e395b32dcb803bd158e1d76106187
Weerayuth Uarjaipra <yut@Yuts-MacBook-Air-M2.local>
2025-02-01 15:44:32 +0700

    Task 6 - No. 1-2

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
-let x = 5;
-let y = 11;
-
-if (x > 5 && y > 12) {
-  console.log("ค่ามากจัง");
-} else {
-  console.log(x * y + x / y);
+let x = 10;
+if (x > 10) {
+  console.log("x is greater than 10");
 }
