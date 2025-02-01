4a76bcc835e350d64bcdf74f6116957468a18dfe
Weerayuth Uarjaipra <yut@Yuts-MacBook-Air-M2.local>
2025-02-01 16:02:07 +0700

    Task 6 - No. 9

--- test.js
+++ test.js
@@ -1,8 +1,22 @@
-let x = 7;
-if (x > 10) {
-  console.log("x is more than 10");
-} else if (x < 5) {
-  console.log("x is less than 5");
-} else {
-  console.log("x is between 5 and 10");
-}
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter grade (0-100): ", function (input) {
+  let grade = Number(input);
+
+  if (grade > 80) {
+    console.log("A");
+  } else if (grade > 60) {
+    console.log("B");
+  } else if (grade > 50) {
+    console.log("C");
+  } else {
+    console.log("F");
+  }
+
+  rl.close();
+});
