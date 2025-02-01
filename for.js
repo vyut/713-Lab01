c3b6891ce1206e10a152a3bc5c0a48506f7406e5
Weerayuth Uarjaipra <yut@Yuts-MacBook-Air-M2.local>
2025-02-01 16:03:45 +0700

    Task 7 - No. 4

--- for.js
+++ for.js
@@ -1,4 +1,4 @@
-let i = 0;
-for (i = 0; i < 10; i++) {
-  console.log(i);
+let color = ["red", "blue", "green", "yellow", "orange", "purple"];
+for (let i = 0; i < color.length; i++) {
+  console.log(color[i]);
 }
