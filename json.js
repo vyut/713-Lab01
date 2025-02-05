31755394d0e376c558d5be40613f5af60b41eeca
Weerayuth Uarjaipra <yut@Yuts-MacBook-Air-M2.local>
2025-02-05 20:21:24 +0700

    Task 11 - No. 4

--- json.js
+++ json.js
@@ -8,5 +8,21 @@ const object = {
 
 // console.log(object.name);
 
-const jsonStr = JSON.stringify(object);
-console.log(jsonStr);
+// const jsonStr = JSON.stringify(object);
+// console.log(jsonStr);
+
+const student = {
+  name: "Yut",
+  age: 35,
+  gpa: 3.5,
+};
+
+const processStudent = (student) => {
+  return student.gpa > 2
+    ? JSON.stringify(student)
+    : `คุณ ${student.firstName} พ้นสภาพ`;
+};
+
+console.log(processStudent(student));
+
+console.log(processStudent({ firstName: "Tao", gpa: 1.99 }));
