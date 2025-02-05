84ba2880ebf2341ee4988150dcfb4abf4744226e
Weerayuth Uarjaipra <yut@Yuts-MacBook-Air-M2.local>
2025-02-05 20:04:16 +0700

    Task 9 - No. 4

--- function.js
+++ function.js
@@ -2,8 +2,12 @@
 
 // console.log(greet("John"));
 
-const multiply = (x, y) => {
-  return x * y;
-};
+// const multiply = (x, y) => {
+//   return x * y;
+// };
 
-console.log(multiply(5, 10));
+// console.log(multiply(5, 10));
+
+const getMax = (a, b) => (a > b ? a : b);
+
+console.log(getMax(10, 20));
