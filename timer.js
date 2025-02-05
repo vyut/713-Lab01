abc31dc58e5306c5e87c4fb1539d198111932c17
Weerayuth Uarjaipra <yut@Yuts-MacBook-Air-M2.local>
2025-02-05 20:40:39 +0700

    Task 13 - No. 2

--- timer.js
+++ timer.js
@@ -1,8 +1,15 @@
-waitAndPrint = (message, delay) => {
-  setTimeout(() => {
-    console.log(message);
+let counter = 1;
+
+const waitAndPrint = (message, delay, stopAfter) => {
+  const intervalId = setInterval(() => {
+    console.log(`${message} ${"world ".repeat(counter)}`);
+    counter++;
   }, delay);
+
+  setTimeout(() => {
+    clearInterval(intervalId);
+    console.log("Stopped printing.");
+  }, stopAfter);
 };
 
-// Example usage:
-waitAndPrint("Hello, World!", 2000);
+waitAndPrint("hello", 3000, 15000);
