1403dd07d6c1fa065deb87158fa093015f517c40
Weerayuth Uarjaipra <yut@Yuts-MacBook-Air-M2.local>
2025-02-05 20:26:55 +0700

    Task 12 - No. 3

--- HigerOrder.js
+++ HigerOrder.js
@@ -2,6 +2,14 @@
 // const square = n.map((num) => num * num);
 // console.log(square);
 
-const n = [1, 2, 3, 4, 5];
-const even = n.filter((num) => num % 2 === 0);
-console.log(even);
+// const n = [1, 2, 3, 4, 5];
+// const even = n.filter((num) => num % 2 === 0);
+// console.log(even);
+
+const numberList = [1, 3, 6, 9, 11, 20, 25];
+
+const processNumbers = (numbers) => {
+  return numbers.filter((num) => num % 3 === 0).map((num) => Math.pow(num, 3));
+};
+
+console.log(processNumbers(numberList));
